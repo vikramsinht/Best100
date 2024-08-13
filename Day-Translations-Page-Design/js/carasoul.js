98e23b05742297c383d4a1735fde496f32b6f96d
vikramsinht <vikramt@sigasystems.com>
2024-08-13 17:41:29 +0530

    Day Translation home page design!

---
+++ Day-Translations-Page-Design/js/carasoul.js
@@ -0,0 +1,28 @@
+jQuery(document).ready(function ($) {
+    jQuery('.owl-carousel').slick({
+        dots: true,
+        arrows: true,
+        infinite: true,
+        speed: 600,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    })
+});
